af2758780a44a2bb4bb337a0d8b94eedefa83780
letronghuy-it <hale.02031982@gmail.com>
2024-09-25 15:45:54 +0700

    deloycode

---
+++ QLNV_Tester/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QLNV_VQT;
+
+namespace QLNV_Tester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestLogin()
+        {
+            var login = new frm_dangnhap();
+
+            Assert.IsNull(login.Username, "Please fill textbox username !!");
+            Assert.IsNull(login.pw, "Please fill textbox password !!");
+        }
+    }
+}
