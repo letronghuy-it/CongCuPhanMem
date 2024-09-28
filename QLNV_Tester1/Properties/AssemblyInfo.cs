2665e3746cef46aee61f7fae425da960ceeb3970
letronghuy-it <hale.02031982@gmail.com>
2024-09-28 17:14:51 +0700

    200

---
+++ QLNV_Tester1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("QLNV_Tester1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QLNV_Tester1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7e58f788-2002-4740-aedd-37f04754937d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ QLNV_Tester1/UniTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QLNV_LTH;  
+
+namespace QLNV_Tester1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestLogin()
+        {
+            // Khởi tạo form đăng nhập
+            var login = new frm_dangnhap();
+
+            // Giả định rằng frm_DangNhap có các thuộc tính công khai cho Username và Password
+            Assert.AreEqual(string.Empty, login.Username, "Vui lòng điền tên người dùng vào ô văn bản!");
+            Assert.AreEqual(string.Empty, login.pw, "Vui lòng điền mật khẩu vào ô văn bản!");
+        }
+    }
+}
